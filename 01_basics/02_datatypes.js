b17cd1b3fe27f3acfeb20c0edb0cd0107acc4a1c
Bhoomi Kathiriya <bhoomi.kathiria@ascendion.com>
2024-05-31 17:35:02 +0530

    Datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3 + 3);  we are using node js not browser
+
+let name = "bhoomi";
+let age = 18;
+let isLoggedIn = true;
+
+//number
+//bigInt
+//string
+// boolean
+// null
+// undefined
+// symbol => Unique
+// object
+
+console.log(typeof null); // Object
+console.log(typeof undefined); //Undefined
