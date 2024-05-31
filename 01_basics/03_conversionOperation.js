b4841033824b472301e8c49b17a0cc3ab5ebec5b
Bhoomi Kathiriya <bhoomi.kathiria@ascendion.com>
2024-05-31 17:48:54 +0530

    conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "Bhoomi";
+
+// console.log(typeof score);
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// 33 => 33
+// "33abc"  => NaN
+// true =>1, false => 0
+
+let isLoggedIn = "";
+
+let booleanisLoggedIn = Boolean(isLoggedIn);
+
+//console.log(booleanisLoggedIn);
+
+// 1 -> True 0-> False
+
+// "" -> false
+// "bhoomi" -> true
+
+let somenumber = 33;
+let stringNumber = String(somenumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
