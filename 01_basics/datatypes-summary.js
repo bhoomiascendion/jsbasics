245334988bf8ac8cb0441f39982f5531b1b05050
Bhoomi Kathiriya <bhoomi.kathiria@ascendion.com>
2024-06-03 15:09:13 +0530

    datatype summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,14 @@
+// primitive
+
+// 7 Types: String, Number, Boolean, null, undefined, symbol, BigInt
+
+//Non primitive
+
+//Reference types:
+
+//Array, Objects, Functions
+
+const id = Symbol("123");
+const anotherId = Symbol("123");
+
+console.log(id === anotherId);
