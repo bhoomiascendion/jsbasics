c4f712d0e7285d520f856d128e302a6b7a572e90
Bhoomi Kathiriya <bhoomi.kathiria@ascendion.com>
2024-06-05 17:32:08 +0530

    Json

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -44,12 +44,26 @@ const users = [
   },
 ];
 
-console.log(users[1].email);
+// console.log(users[1].email);
 
-console.log(tinderUser);
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty("isLoggedIn"));
+// console.log(tinderUser.hasOwnProperty("isLoggedIn"));
+
+const course = {
+  coursename: "javascript",
+  price: "999",
+  courseInstructor: "Mark",
+};
+
+const { courseInstructor: instructor } = course;
+
+console.log(instructor);
+
+// const navbar = ({ company }) => {};
+
+// navbar((company = "bhoomi"));
