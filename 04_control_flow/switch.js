65c12c2817bc1bba57db05b76d0f06e32a82bc45
Bhoomi Kathiriya <bhoomi.kathiria@ascendion.com>
2024-06-06 16:42:17 +0530

    Control flow

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,28 @@
+// switch (key) {
+//   case value:
+//     break;
+
+//   default:
+//     break;
+// }
+
+const month = "feb";
+
+switch (month) {
+  case "jan":
+    console.log("Jan");
+    break;
+  case "feb":
+    console.log("feb");
+    break;
+  case "mar":
+    console.log("mar");
+    break;
+  case "apr":
+    console.log("apr");
+    break;
+
+  default:
+    console.log("default");
+    break;
+}
