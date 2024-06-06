142902295221e9b5f05ff3450316ea5580ec40c7
Bhoomi Kathiriya <bhoomi.kathiria@ascendion.com>
2024-06-06 11:42:42 +0530

    Scope

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,11 +1,52 @@
 //var c = 300;
 
+const webdriver = require("wd/lib/webdriver");
+
 let a = 300;
 if (true) {
   let a = 10;
   const b = 20;
-  console.log("INNER", a);
+  //console.log("INNER", a);
 }
 //console.log(a);
 //console.log(b);
-console.log(a);
+//console.log(a);
+
+function one() {
+  const username = "hitesh";
+
+  function two() {
+    const website = "youtube";
+    // console.log(username);
+  }
+  //  console.log(website);
+  // two();
+}
+
+one();
+
+if (true) {
+  const username = "hitesh";
+  if (username == "hitesh") {
+    const website = "youtube";
+    // console.log(username + website);
+  }
+
+  // console.log(website);
+}
+
+//console.log(username);
+
+//++ Interesting
+
+console.log(addone(5));
+
+function addone(num) {
+  return num + 1;
+}
+
+addTwo(5);
+
+const addTwo = function (num) {
+  return num + 2;
+};
